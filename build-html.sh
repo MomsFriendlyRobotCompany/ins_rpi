410f8615af683b3052bdf0cfdcba0ae5ceb904b3
walchko <walchko@users.noreply.github.com>
2018-10-13 10:20:45 -0600

    up

--- build-html.sh
+++ build-html.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # pandoc -t html5 -s -c docs/pandoc.css -H docs/header.html readme.md -o index.html
-pandoc -t html5 -s -c docs/pandoc.css -A docs/footer.html readme.md -o index.html
+# pandoc -t html5 -s -c docs/pandoc.css -A docs/footer.html readme.md -o index.html
+pandoc -t html5 -s -c docs/pandoc.css readme.md -o index.html
 pandoc -t html5 -s  assembly.md -o assembly.html
